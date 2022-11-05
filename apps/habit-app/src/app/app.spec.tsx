a69297d6b2311134c5ca45f47a009136820efeb8
Su Anli <su.anli@anlitz.com>
2022-11-05 18:34:41 +0800

    chore(repo): setup test with coverage

--- apps/habit-app/src/app/app.spec.tsx
+++ apps/habit-app/src/app/app.spec.tsx
@@ -4,6 +4,6 @@ import { render } from '@testing-library/react-native';
 import App from './app';
 
 test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome')).toBeDefined();
 });
