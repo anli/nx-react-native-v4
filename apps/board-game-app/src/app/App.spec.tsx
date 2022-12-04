1f9c79f233ef165f165740e0b4d592c2a78ce777
Su Anli <su.anli@anlitz.com>
2022-12-04 16:29:23 +0800

    chore(board-game-app): setup test with coverage

--- apps/board-game-app/src/app/App.spec.tsx
+++ apps/board-game-app/src/app/App.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import App from './App';
+import App from './app';
 
 test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome')).toBeDefined();
 });
