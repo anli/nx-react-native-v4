ccc5e98297d9ea23b304c20364b3fecec35d1295
Su Anli <su.anli@anlitz.com>
2022-12-04 16:47:49 +0800

    chore(board-game-app): add ts-standard to lint

--- apps/board-game-app/src/app/app.tsx
+++ apps/board-game-app/src/app/app.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Welcome</Text>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- apps/board-game-app/src/main.tsx
+++ apps/board-game-app/src/main.tsx
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './app/app';
+import { AppRegistry } from 'react-native'
+import App from './app/app'
 
-AppRegistry.registerComponent('BoardGame', () => App);
+AppRegistry.registerComponent('BoardGame', () => App)
