111b1da3c2f0987da42b5be87a99fcf60f9bd899
Su Anli <anli@users.noreply.github.com>
2022-12-04 17:11:19 +0800

    chore(nx): add board-game-app, board-game-app-e2e (#10)

---
+++ apps/board-game-app/src/app/App.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+
+import App from './app'
+
+test('renders correctly', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('Welcome')).toBeDefined()
+})
