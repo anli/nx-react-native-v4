20ab5586e0469f721c630a7e508fedbc681eb726
Su Anli <su.anli@anlitz.com>
2022-12-04 16:55:44 +0800

    fix(board-game-app): fix type-check

--- apps/board-game-app/src/app/app.tsx
+++ apps/board-game-app/src/app/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
-const App = () => {
+const App = (): React.ReactElement => {
   return (
     <SafeAreaView>
       <Text>Welcome</Text>
